c24ab9af0c131a6031a8bba7cb0f26efa2fbd51b
Mohamed Aziz NJAIMI <manjaimi@vermeg.com>
2022-06-01 17:26:10 +0100

    Add Candidat

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CandidateComponent } from './candidate/candidate.component';
+import { ListComponent } from './candidate/components/list/list.component';
+import { AddComponent } from './candidate/components/add/add.component';
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CandidateComponent
+    CandidateComponent,
+    ListComponent,
+    AddComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
