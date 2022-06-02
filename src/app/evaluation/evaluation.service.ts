33676b1206c79fcb6e535a5ad6d87090aa249b5a
sneji <sneji@vermeg.com>
2022-06-02 17:04:55 +0100

    Aziz work

--- src/app/evaluation/evaluation.service.ts
+++ src/app/evaluation/evaluation.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {Observable} from "rxjs";
 import {Evaluation} from "../models/Evaluation";
+import {environment} from "../../environments/environment";
 
 @Injectable({
   providedIn: 'root'
@@ -12,14 +13,18 @@ export class EvaluationService {
   }
 
   getEvaluations(): Observable<any> {
-    return this.http.get<Evaluation>('assets/mock/evaluation.json')
+    return this.http.get<Evaluation>('/evaluations')
   }
 
   saveEval(body){
-    return this.http.post('url',body)
+    return this.http.post('/evaluations',body)
+  }
+
+  editEval(body){
+    return this.http.put(environment.basePath+'/evaluations',body)
   }
 
   getEval(id){
-    return this.http.get<Evaluation>('assets/mock/evaluation/'+id)
+    return this.http.get<Evaluation>(environment.basePath+'/evaluations'+id)
   }
 }
